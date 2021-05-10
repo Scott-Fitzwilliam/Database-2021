b2f8b0c7818d929271e0f85e9e18e2c4a5b19ed8
Scott-Fitzwilliam <103116463@student.swin.edu.au>
2021-05-10 14:30:02 +1000

    Task2 Completed

--- Assessment 10_05_2021.sql
+++ Assessment 10_05_2021.sql
@@ -1,5 +1,4 @@
 -- Scott Fitzwilliam, 103116463, Group 2.
-USE Assessment10_05_2021
 
 -- TOUR (TourName, Description)
 -- PRIMARY KEY (TourName)
@@ -11,7 +10,59 @@ USE Assessment10_05_2021
 -- PRIMARY KEY(TourName, EventYear, EventMonth, EventDay)
 -- FOREIGN KEY(TourName) REFERENCES TOUR
 
--- Booking(ClientID, EventYear, EventMonth, EventDay, DateBooked, Payment)
--- PRIMARY KEY(ClientID, EventYear, EventMonth, EventDay)
+-- Booking(ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment)
+-- PRIMARY KEY(ClientID, TourName, EventYear, EventMonth, EventDay)
 -- FOREIGN KEY(ClientID) REFERENCES Client
 -- FOREIGN KEY(TourName, EventYear, EventMonth, EventDay) REFERENCES Event
+
+USE Assessment10_05_2021
+-- The drop order has to be reversed due to data integrity
+IF OBJECT_ID('Booking') IS NOT NULL
+DROP TABLE Booking;
+IF OBJECT_ID('Event') IS NOT NULL
+DROP TABLE [Event];
+IF OBJECT_ID('Client') IS NOT NULL
+DROP TABLE Client;
+IF OBJECT_ID('TOUR') IS NOT NULL
+DROP TABLE TOUR;
+
+GO
+CREATE TABLE TOUR(
+  TourName NVARCHAR(100)
+, [Description] NVARCHAR(500)
+, PRIMARY KEY (TourName)
+);
+
+CREATE TABLE CLIENT(
+  ClientID INT
+, Surname NVARCHAR(100) NOT NULL
+, GivenName NVARCHAR(100) NOT NULL
+, Gender NVARCHAR(1) CHECK (Gender IN ('M','F','I')) 
+, PRIMARY KEY (ClientID)
+);
+
+CREATE TABLE [Event](
+  TourName NVARCHAR(100)
+, EventYear INT CHECK (EventYear between 0000 AND 9999)
+, EventMonth NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', '[Dec]'))
+, EventDay INT CHECK (EventDay >= 1 and EventDay <= 31)
+, Fee MONEY NOT NULL, CHECK (Fee > 0)
+, PRIMARY KEY (TourName, EventYear, EventMonth, EventDay)
+, FOREIGN KEY (TourName) REFERENCES TOUR
+);
+
+CREATE TABLE Booking(
+  ClientID INT
+, TourName NVARCHAR(100)
+, EventYear INT CHECK (EventYear between 0000 AND 9999)
+, EventMonth NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', '[Dec]'))
+, EventDay INT CHECK(EventDay >= 1 and EventDay <= 31)
+, DateBooked DATE NOT NULL
+, Payment MONEY CHECK (Payment > 0) 
+, PRIMARY KEY (ClientID, TourName, EventYear, EventMonth, EventDay)
+, FOREIGN KEY (ClientID) REFERENCES Client
+, FOREIGN KEY (TourName, EventYear, EventMonth, EventDay) REFERENCES [Event]
+);
+
+select table_name from information_schema.tables;
+
